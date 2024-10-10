5e85f5a380422e44749cf12006cc4c4b4e2997f3
Miguel Rabatini Dias Santana <rabatinimiguel@gmail.com>
2024-10-10 16:54:20 -0300

    Cleanup and modify App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <>
+      <header className="h-14 min-w-80 pl-4 text-5xl">Pokédex</header>
       <Outlet />
+      <footer className="min-w-80 py-1 text-center text-xs">
+        Made by Miguel Rabatini Dias Santana
+      </footer>
     </>
   );
 };
