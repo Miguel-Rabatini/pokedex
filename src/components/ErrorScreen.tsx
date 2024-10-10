cefe5488d08e7afdf867402f4944952b744104df
Miguel Rabatini Dias Santana <rabatinimiguel@gmail.com>
2024-10-10 16:54:33 -0300

    Create ErrorScreen component.

---
+++ src/components/ErrorScreen.tsx
@@ -0,0 +1,11 @@
+const ErrorScreen = () => {
+  return (
+    <main className="grid h-page min-h-60 min-w-80 place-items-center px-4 text-center text-xl">
+      <span className="rounded-full bg-white px-4 py-1 text-rose-600 shadow-md shadow-black/20">
+        An error ocurred. Please, try again later.
+      </span>
+    </main>
+  );
+};
+
+export default ErrorScreen;
