5f084a6bfd8e08486a4dd6ca3460a746ab0cb2cd
Miguel Rabatini Dias Santana <rabatinimiguel@gmail.com>
2024-10-10 16:55:00 -0300

    Create LoadingScreen component.

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,11 @@
+const LoadingScreen = () => {
+  return (
+    <main className="grid h-page min-h-60 min-w-80 place-items-center">
+      <span className="rounded-full bg-white px-4 py-2 text-2xl text-rose-600 shadow-md shadow-black/20">
+        Loading...
+      </span>
+    </main>
+  );
+};
+
+export default LoadingScreen;
