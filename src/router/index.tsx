ba18ddec115fe8eb2355ff03edaab2b132bb06d9
Miguel Rabatini Dias Santana <rabatinimiguel@gmail.com>
2024-10-10 16:58:27 -0300

    Create router.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,25 @@
 // react-router-dom
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 // Components
 import App from "../App";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+// Pages
+import ErrorPage from "../pages/ErrorPage";
+import PokedexPage from "../pages/PokedexPage";
+import PokemonInfoPage from "../pages/PokemonInfoPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Navigate to="pokedex" /> },
+      { path: "pokedex/:pokemonName", element: <PokemonInfoPage /> },
+      { path: "pokedex", element: <PokedexPage /> },
+    ],
+  },
+]);
 
 export default router;
